7e00e1fabcc79bd325235ffafbd84eaef46cc6bb
IshaanVadhan <ishaanvadhan2001@gmail.com>
2024-04-09 13:40:01 +0530

    interceptors

---
+++ src/main/java/springmvc/controller/MyInterceptor.java
@@ -0,0 +1,34 @@
+package springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("This is prehandler!");
+		String name = (String) request.getAttribute("name");
+		System.out.println(name);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("This is posthandler!");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("This is aftercompletion!");
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+}
